87b91370e47492d9704535302d06c51705e6773e
Trishachowdary <121429555+Trishachowdary@users.noreply.github.com>
2023-01-02 16:12:26 +0530

    Update main.py

--- main.py
+++ main.py
@@ -1,10 +1,26 @@
-import boto3
-ec2 = boto3.resource('ec2')
+# Program to check if a number is prime or not
 
-instances = ec2.create_instances(
-        ImageId="ami-0dafa01c8100180f8",
-        MinCount=1,
-        MaxCount=1,
-        InstanceType="t2.micro",
-        KeyName="KeyPair1"
-    )
+num = 29
+
+# To take input from the user
+#num = int(input("Enter a number: "))
+
+# define a flag variable
+flag = False
+
+if num == 1:
+    print(num, "is not a prime number")
+elif num > 1:
+    # check for factors
+    for i in range(2, num):
+        if (num % i) == 0:
+            # if factor is found, set flag to True
+            flag = True
+            # break out of loop
+            break
+
+    # check if flag is True
+    if flag:
+        print(num, "is not a prime number")
+    else:
+        print(num, "is a prime number")
